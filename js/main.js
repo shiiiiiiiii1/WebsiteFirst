978b402e93a4139bd842bba474e5ef909159dc65
shoichi <shou1_822@yahoo.co.jp>
2016-06-28 16:40:30 +0900

    popup

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  $('.large-img-btn').bind('click', function() {
+    var img_path = $(this).attr('imgpath');
+    $('.cover-bg').fadeIn('slow/400/fast', function() {
+      $('.popup-img').attr('src', img_path);
+    });
+  });
+  $('.cover-bg').bind('click', function() {
+    $('.cover-bg').fadeOut('slow/400/fast', function() {
+    });
+  });
+});
