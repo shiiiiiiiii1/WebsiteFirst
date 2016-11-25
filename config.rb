dd492ea796d833a199e9cbc9c8b54f3e5d40f025
shoichi yokobori <shoichi@MacBook-Pro-shoichi.local>
2016-11-26 02:46:40 +0900

    create docs folder

--- config.rb
+++
@@ -1,6 +0,0 @@
-http_path = "/"
-css_dir = "css"		#Outputする場所の設定
-sass_dir = "scss"	#保存されているフォルダ
-output_style = :nested	#吐き出されるパターン
-line_comments = false
-sass_options = { :sourcemap => true }	#souce.mapの吐き出し
---
+++ docs/config.rb
@@ -0,0 +1,6 @@
+http_path = "/"
+css_dir = "css"   # Outputする場所の設定
+sass_dir = "scss"   # 保存されているフォルダ
+output_style = :nested   # 吐き出されるパターン
+line_comments = false
+sass_options = { :sourcemap => true }   # souce.mapの吐き出し
